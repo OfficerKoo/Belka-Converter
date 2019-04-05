813a8b2a479d5033d54a61f09165c4bb52e226d8
Aleksei Klopkov <Aleksei_Klopkov@epam.com>
2019-04-05 14:32:51 +0300

    fixed empty rates.py

---
+++ src/BelkaConverter/rates.py
@@ -0,0 +1,12 @@
+import urllib3
+import json
+
+URL = 'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'
+
+
+def get_rates():
+    http = urllib3.PoolManager()
+    r = http.request('GET', URL)
+    rates = json.loads(r.data.decode('utf-8'))
+    return rates
+

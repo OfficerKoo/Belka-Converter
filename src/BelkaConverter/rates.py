2df8f0056e9d12673a9917f2e737337d94f58607
Aleksei Klopkov <Aleksei_Klopkov@epam.com>
2019-04-08 15:44:52 +0300

    Added exchange function

--- src/BelkaConverter/rates.py
+++ src/BelkaConverter/rates.py
@@ -10,3 +10,9 @@ def get_rates():
     rates = json.loads(r.data.decode('utf-8'))
     return rates
 
+def exchange(base, cur1, cur2, scale):
+    if base == 'BYN':
+        result = cur1 / cur2 * scale
+    else:
+        result = cur2 * cur1 / scale
+    return result

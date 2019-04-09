4aef40457c43d0e5a33e7c680f8280d1d0a6bbe1
Aleksei Klopkov <Aleksei_Klopkov@epam.com>
2019-04-09 16:28:14 +0300

    finished

--- src/BelkaConverter/cli.py
+++ src/BelkaConverter/cli.py
@@ -3,9 +3,29 @@ from argparse import ArgumentParser
 def create_parser():
     parser = ArgumentParser()
     pos_group = parser.add_argument_group()
-    pos_group.add_argument('amount', type=float, help='Enter the ammount of currency to convert')
-    pos_group.add_argument('currency',
+    pos_group.add_argument('amount', 
+     type=float, 
+     help='Enter the ammount of currency to convert', 
+     required=True
+     )
+    pos_group.add_argument('currency', default=['BYN', 'RUB'],
      nargs=2, metavar=('CUR1','CUR2'), 
-     help='Enter the currencies to convert like so from CUR1 to CUR2',
+     help='Enter the currencies to convert like so from CUR1 to CUR2, default is BYN to RUB',
      )
     return parser
+
+def main():
+     from BelkaConverter import rates
+
+     args = create_parser().parse_args()
+     rates = rates.get_rates()
+     curs = []
+     for obj in rates:
+          if obj['Cur_Abbreviation'] in args.currency:
+               curs.append(obj)
+     if args.currency[0] == 'BYN':
+          result = args.ammount / curs[0]['Cur_OfficialRate'] * curs[0]['Cur_Scale']
+     else:
+          result = curs[0]['Cur_OfficialRate'] * args.ammount / curs[0]['Cur_Scale']
+
+     print(f"For {args.ammount} of {args.currency[0]} you get {result} of {args.currency[1]}")
--- src/BelkaConverter/rates.py
+++ src/BelkaConverter/rates.py
@@ -9,10 +9,3 @@ def get_rates():
     r = http.request('GET', URL)
     rates = json.loads(r.data.decode('utf-8'))
     return rates
-
-def exchange(base, cur1, cur2, scale):
-    if base == 'BYN':
-        result = cur1 / cur2 * scale
-    else:
-        result = cur2 * cur1 / scale
-    return result

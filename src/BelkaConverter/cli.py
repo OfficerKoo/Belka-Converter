ff62f7ea47bf35d016e212a515ce5a18cd3368df
Aleksei Klopkov <Aleksei_Klopkov@epam.com>
2019-04-09 14:22:37 +0300

    fixed argparse

--- src/BelkaConverter/cli.py
+++ src/BelkaConverter/cli.py
@@ -4,8 +4,8 @@ def create_parser():
     parser = ArgumentParser()
     pos_group = parser.add_argument_group()
     pos_group.add_argument('amount', type=float, help='Enter the ammount of currency to convert')
-    pos_group.add_argument('currency', default=('BYN', 'RUB'),
+    pos_group.add_argument('currency',
      nargs=2, metavar=('CUR1','CUR2'), 
-     help='Enter the currencies to convert like so from CUR1 to CUR2'
+     help='Enter the currencies to convert like so from CUR1 to CUR2',
      )
     return parser
